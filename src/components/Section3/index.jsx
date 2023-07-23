af3e5ab5ba917e80ed1677aac249a9f3d2e7daba
stavbers <reddevweb@gmail.com>
2023-07-24 00:32:36 +0300

    add section pos absolute

--- src/components/Section3/index.jsx
+++ src/components/Section3/index.jsx
@@ -1,7 +1,22 @@
 import st from './Section3.module.css'
 
 function Section3(props){
-  return
+  console.log(props.data)
+  const {logo, descr, info, priceTip, title} = props.data
+  
+  return (
+    <div className="container">
+      <div className={st.wrp}>
+        <img className={st.img} src={logo} alt={descr} />
+        <span className={st.left}></span>
+        <span className={st.right}></span>
+        <span className={st.title}>{title}</span>
+        <span className={st.btn}>{info}</span>
+        <span className={st.tip}></span>
+        <p className={st.info}>{descr}</p>
+      </div>
+    </div>
+  )
 }
 
 export {Section3}
