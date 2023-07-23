844a1a57466375c36b30f975b837892742aa6684
stavbers <reddevweb@gmail.com>
2023-07-23 23:44:39 +0300

    some fix

--- src/components/Section2/index.jsx
+++ src/components/Section2/index.jsx
@@ -1,16 +1,20 @@
-import st from './Section2.module.css'
-function Section2(props){
-  console.log(props.data)
+import st from './Section2.module.css';
+function Section2(props) {
+    console.log(props.data);
 
-  
-  return (
-    <div className="container">
-        <div className={st.frame}>
-        {props.data.map((el, indx)=> {
-          return <div key={indx}><img src={el.img} alt="el.title" /> <p>{el.title}</p></div>
-        })}
+    return (
+        <div className='container'>
+            <div className={st.frame}>
+                {props.data.map((el, indx) => {
+                    return (
+                        <div className={st.block} key={indx}>
+                            <img className={st.img} src={el.img} alt={el.title} />
+                            <p className={st.info}>{el.title}</p>
+                        </div>
+                    );
+                })}
+            </div>
         </div>
-    </div>
-  )
+    );
 }
-export {Section2}
+export { Section2 };
