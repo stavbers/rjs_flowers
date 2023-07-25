e17d8cbbe89ec8f2b19b73d24ef3e10c7bc296dd
stavbers <reddevweb@gmail.com>
2023-07-25 20:11:47 +0300

    ebanaya ainimation!

--- src/components/Section3/index.jsx
+++ src/components/Section3/index.jsx
@@ -1,22 +1,44 @@
-import st from './Section3.module.css'
+import React, { useState, useRef } from 'react';
+import st from './Section3.module.css';
 
-function Section3(props){
-  console.log(props.data)
-  const {logo, descr, info, priceTip, title} = props.data
-  
-  return (
-    <div className="container">
-      <div className={st.wrp}>
-        <img className={st.img} src={logo} alt={descr} />
-        <span className={st.left}></span>
-        <span className={st.right}></span>
-        <span className={st.title}>{title}</span>
-        <span className={st.btn}>{info}</span>
-        <span className={st.tip}></span>
-        <p className={st.info}>{descr}</p>
-      </div>
-    </div>
-  )
+import { CSSTransition } from 'react-transition-group';
+
+function Section3(props) {
+    const [showMessage, setShowMessage] = useState(false);
+    const nodeRef = useRef(null);
+    console.log(props.data);
+    const { logo, descr, info, priceTip, title } = props.data;
+    
+
+    
+    return (
+        <div className='container'>
+            <div className={st.wrp}>
+                <img className={st.img} src={logo} alt={descr} />
+
+                <div className={st.title}>
+                    {title}
+                    <span className={st.left}></span>
+                    <span className={st.right}></span>
+                    <span className={st.btn} onClick={() => setShowMessage((prev)=> !prev)}>
+                        {info}
+                    </span>
+                    <span className={st.tip}></span>
+                </div>
+                <CSSTransition
+                in={showMessage}
+                nodeRef={nodeRef}
+                timeout={300}
+                classNames='alert'
+                unmountOnExit
+                onEnter={() => setShowMessage(true)}
+                onExited={() => setShowMessage(false)}
+                >
+                <p className={st.info} ref={nodeRef}>{descr}</p>
+                </CSSTransition>   
+            </div>
+        </div>
+    );
 }
 
-export {Section3}
+export { Section3 };
