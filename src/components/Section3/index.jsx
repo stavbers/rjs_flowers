83f3da882474b0a88a4e6cff3ec40f8baa825970
stavbers <reddevweb@gmail.com>
2023-07-25 20:38:55 +0300

    eeebannayyyaaaa animation 2

--- src/components/Section3/index.jsx
+++ src/components/Section3/index.jsx
@@ -1,41 +1,31 @@
 import React, { useState, useRef } from 'react';
 import st from './Section3.module.css';
 
-import { CSSTransition } from 'react-transition-group';
 
 function Section3(props) {
-    const [showMessage, setShowMessage] = useState(false);
-    const nodeRef = useRef(null);
+
+
     console.log(props.data);
     const { logo, descr, info, priceTip, title } = props.data;
-    
 
-    
     return (
         <div className='container'>
             <div className={st.wrp}>
                 <img className={st.img} src={logo} alt={descr} />
-
                 <div className={st.title}>
                     {title}
                     <span className={st.left}></span>
                     <span className={st.right}></span>
-                    <span className={st.btn} onClick={() => setShowMessage((prev)=> !prev)}>
+                    <span
+                        className={st.btn}
+                    >
                         {info}
                     </span>
                     <span className={st.tip}></span>
                 </div>
-                <CSSTransition
-                in={showMessage}
-                nodeRef={nodeRef}
-                timeout={300}
-                classNames='alert'
-                unmountOnExit
-                onEnter={() => setShowMessage(true)}
-                onExited={() => setShowMessage(false)}
-                >
-                <p className={st.info} ref={nodeRef}>{descr}</p>
-                </CSSTransition>   
+                <p className={st.info} >
+                        {descr}
+                    </p>
             </div>
         </div>
     );
