6fb4cfd6d63eb7851d80e4a5991ad6910b803017
stavbers <reddevweb@gmail.com>
2023-08-01 23:10:28 +0300

    fix extra long text

--- src/components/FlowersHeader/index.jsx
+++ src/components/FlowersHeader/index.jsx
@@ -1,12 +1,24 @@
+import React, {useRef, useEffect} from 'react'
+
 import st from './FlowersHeader.module.css';
 
 
 function FlowersHeader({logo, descr, info, title, handleClick, inProp}){
+  const subTitleRef = useRef(null)
+
+  useEffect(()=> {
+
+      if(title.length > 13){
+        subTitleRef.current.classList.add(st['smText'])
+      }
+  }, [])  
+
+  
   return(
     <div className={st.wrp}>
     <img className={st.img} src={logo} alt={descr} />
     <div className={st.title}>
-        <p className={st.subTitle}>{title}</p>
+        <p ref={subTitleRef} className={st.subTitle}>{title}</p>
         <span className={st.hr}></span> 
         <span className={st.btn} onClick={handleClick}>
             {inProp ? 'Скрыть' : info}
